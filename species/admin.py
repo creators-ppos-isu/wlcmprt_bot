c7c49f5bb124a6be70c10f2951cae75ee88469f4
Никита <ruvemaximus@gmail.com>
2024-08-26 21:50:46 +0800

    improve(specie): inline  в админке

--- species/admin.py
+++ species/admin.py
@@ -2,12 +2,19 @@ from django.contrib import admin
 from .models import Specie, SpeciePhoto
 
 
+class SpeciePhotoInline(admin.StackedInline):
+    model = SpeciePhoto
+    extra = 1
+
+
 @admin.register(Specie)
 class SpeciesAdmin(admin.ModelAdmin):
     list_display = ("title", "participants_left")
     search_fields = ("title",)
+    inlines = [SpeciePhotoInline]
 
-@admin.register(SpeciePhoto)
-class SpeciePhotoAdmin(admin.ModelAdmin):
-    list_display = ("specie", "photo")
-    search_fields = ("specie",)
+
+# @admin.register(SpeciePhoto)
+# class SpeciePhotoAdmin(admin.ModelAdmin):
+#     list_display = ("specie", "photo")
+#     search_fields = ("specie",)
